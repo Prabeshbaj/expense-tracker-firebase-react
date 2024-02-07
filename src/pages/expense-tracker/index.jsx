d2a73c2939c5b553c97109a81407bdc60d539f71
Prabeshbaj <38983508+Prabeshbaj@users.noreply.github.com>
2024-02-06 22:37:43 -0500

    updated bootstrap and font

--- src/pages/expense-tracker/index.jsx
+++ src/pages/expense-tracker/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAddTransaction } from "../../hooks/useAddTransaction";
 import { useGetTransactions } from "../../hooks/useGetTransactions";
-import {useGetUserInfo} from "../../hooks/useGetUserInfo";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import "./styles.css";
 import { signOut } from "firebase/auth";
 import { auth } from "../../config/firebase-config";
@@ -12,18 +12,18 @@ export const ExpenseTracker = () => {
     const { addTransaction } = useAddTransaction();
     const [description, setDescription] = useState("");
     const { transactions, transactionsTotals } = useGetTransactions();
-    const {name, profilePhoto} = useGetUserInfo();
+    const { name, profilePhoto } = useGetUserInfo();
     const [transactionAmount, setTransactionAmount] = useState(0);
     const [transactionType, setTransactionType] = useState("expense");
 
     const { balance, income, expenses } = transactionsTotals;
-    const signUserOut = async()=> {
+    const signUserOut = async () => {
         try {
-        await signOut(auth);
-        localStorage.clear();
-        navigate("/");
-    }
-        catch(err) {
+            await signOut(auth);
+            localStorage.clear();
+            navigate("/");
+        }
+        catch (err) {
             console.error(err);
         }
     }
@@ -37,48 +37,69 @@ export const ExpenseTracker = () => {
 
     return (
         <>
-            <div className="expense-tracker">
+            <div class="container-md">
 
                 <div className="container">
-                    <h1> {name}'s Expense Tracker</h1>
-                    <div className="balance">
-                        <h3>Your balance</h3>
-                       {balance >=0 ? (<h2> ${balance}</h2>): (<h2>-${balance * -1} </h2>)} 
-                    </div>
-                    <div className="summary">
-                        <div className="income">
-                            <h4>Income</h4>
-                            <p>${income}</p>
+                    <div class="row">
+                        <div class="col-sm text-center">
+                            <h1> {name}'s Expense Tracker</h1> </div>
+                        <div class="col-sm">
+                            {profilePhoto && <div className="profile"><img className="profile-photo" src={profilePhoto} alt="" /> </div>}
                         </div>
-                        <div className="expenses">
-                            <h4>Expenses</h4>
-                            <p>${expenses}</p>
+                        <div class="col-sm text-right">
+                            <button className="btn btn-danger" onClick={signUserOut}>Sign Out</button>
+                        </div> 
+
+                        <div className="balance">
+                            <h3>Your balance</h3>
+                            {balance >= 0 ? (<h2> ${balance}</h2>) : (<h2>-${balance * -1} </h2>)}
                         </div>
+                        <div className="summary">
+                            <div className="income">
+                                <h4>Income</h4>
+                                <p>${income}</p>
+                            </div>
+                            <div className="expenses">
+                                <h4>Expenses</h4>
+                                <p>${expenses}</p>
+                            </div>
+                        </div>
+                        <form action="" className="" onSubmit={onSubmit}>
+                        <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                            <input type="text" class="form-control" placeholder="Description" value={description} id="description" required onChange={(e) => setDescription(e.target.value)} />
+                            </div>
+                            <div class="mb-3">
+                            <label for="amount" class="form-label">Amount</label>
+                            <input type="number" class="form-control" placeholder="Amount" required value={transactionAmount} id="amount" onChange={(e) => setTransactionAmount(e.target.value)} />
+                            </div>
+                            <div class="mb-3 form-check">
+                            <input type="radio" class="form-check-input" id="expense" value="expense" checked={transactionType === "expense"} onChange={(e) => setTransactionType(e.target.value)} />
+                            <label htmlFor="expense">Expense</label>
+                            </div>
+                            <div class="mb-3 form-check">
+                            <input type="radio" class="form-check-input" id="income" value="income" checked={transactionType === "income"} onChange={(e) => setTransactionType(e.target.value)} />
+                            <label htmlFor="income">Income</label>
+                            </div>
+                            
+                            <button type="submit" class="btn btn-success">Add Transaction</button>
+                        </form>
                     </div>
-                    <form action="" className="add-transaction" onSubmit={onSubmit}>
-                        <input type="text" placeholder="Description" value={description} required onChange={(e) => setDescription(e.target.value)} />
-                        <input type="number" placeholder="Amount" required value={transactionAmount} onChange={(e) => setTransactionAmount(e.target.value)} />
-                        <input type="radio" id="expense" value="expense" checked={transactionType === "expense"} onChange={(e) => setTransactionType(e.target.value)} />
-                        <label htmlFor="expense">Expense</label>
-                        <input type="radio" id="income" value="income" checked={transactionType === "income"} onChange={(e) => setTransactionType(e.target.value)} />
-                        <label htmlFor="income">Income</label>
-                        <button type="submit">Add Transaction</button>
-                    </form>
+
+
                 </div>
-                {profilePhoto && <div className="profile"><img className="profile-photo" src={profilePhoto} alt="" /> </div>} <br></br>
-                <button className="sign-out-button" onClick={signUserOut}>Sign Out</button>
-            </div>
-            <div className="transactions">
-                <h3>Transactions</h3>
-                <ul>
-                    {transactions.map((transaction) => {
-                        const { description, transactionAmount, transactionType } = transaction;
-                        return <li>
-                            <h4>{description}</h4>
-                            <p>${transactionAmount} - <label style={{color: transactionType === "expense" ? "red" :"green"}}>{transactionType}</label></p>
-                        </li>
-                    })}
-                </ul>
+                <div className="transactions">
+                    <h3>Transactions</h3>
+                    <ul>
+                        {transactions.map((transaction) => {
+                            const { description, transactionAmount, transactionType } = transaction;
+                            return <li>
+                                <h4>{description}</h4>
+                                <p>${transactionAmount} - <label style={{ color: transactionType === "expense" ? "red" : "green" }}>{transactionType}</label></p>
+                            </li>
+                        })}
+                    </ul>
+                </div>
             </div>
         </>
     );
